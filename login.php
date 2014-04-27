7360c8184a0748fba8176e3853cbf3b82de9bd9e
mpietro40 <pietromaffi@rocketmail.com>
2014-04-27 10:47:13 +0200

    Update login.php
    Added html5 tags

--- login.php
+++ login.php
@@ -1,14 +1,26 @@
+<!doctype html>
 <html>
-	<head>
-		<title>My first PHP website</title>
-	</head>
-	<body>
-		<h2>Login Page</h2>
-		<a href="index.php">Click here to go back</a><br/><br/>
+    <head>
+        <meta charset="UTF-8">
+        <title>My PHP Website</title>
+    </head>
+    <body>
+    <header>
+        <h2>Login Page</h2>
+    </header>
+    <nav>    
+        <a href="index.php">Click here to go back</a><br/>
+    </nav>
+    <section>
 		<form action="checklogin.php" method="post">
 			Enter Username: <input type="text" name="username" required="required"/> <br/>
 			Enter Password: <input type="password" name="password" required="required" /> <br/>
 			<input type="submit" value="Login"/>
 		</form>
-	</body>
+    </section>
+    <aside>
+    </aside>
+    <footer>
+    </footer>      
+   </body>
 </html>
