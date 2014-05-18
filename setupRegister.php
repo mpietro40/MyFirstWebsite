43e23e20a8ffe708b841e37e0db66114c3eefbef
mpietro40 <pietromaffi@rocketmail.com>
2014-05-18 14:44:47 +0200

    update setpRegister with some fix
    Added default value

--- setupRegister.php
+++ setupRegister.php
@@ -44,9 +44,10 @@ echo "<h3>Prepare DB creating table presenze</h3>";
 //Add a new table to the database called users
 $sql="CREATE TABLE IF NOT EXISTS presence (
   id int(11) NOT NULL AUTO_INCREMENT,
-  id_classe varchar(50) NOT NULL,
-  presenza INT NOT NULL,
-  day NOT NULL,
+  id_users varchar(50) NOT NULL,
+  data Date NOT NULL,
+  hour_in varchar(8) NOT NULL default '08:30:00',
+  hour_out varchar(8) NOT NULL,
   PRIMARY KEY (id)
 ) ENGINE=InnoDB AUTO_INCREMENT=2";
 
@@ -62,9 +63,9 @@ echo "<h2>Table presence created</h2>";
 
 // Set up database insert some fake data
 echo "<h3>Insert some presence</h3>";
-$sql="INSERT INTO presence (username, password,nome,cognome,indirizzo) VALUES ('pietro', '123456','paolo','rossi','via pallino, milano'),".
-"('paolo', '123456','giacomo','verdi','via pinco, roma'),".
-"('gino', '123456','roberto','bianchi','via rossi, genova')";
+$sql="INSERT INTO presence (id_users, data, hour_in,hour_out) VALUES ('1', Date('11/11/2013'),'08:30','13:30'),".
+"('1', Date('12/11/2013'),'08:30','13:30'),".
+"('1', Date('13/11/2013'),'08:30','13:30')";
 // stampo la query che eseguiro'
 echo "<h3>Setting up presence</h3>";
 echo "$sql"."<br>";
@@ -98,50 +99,13 @@ else {
 print "<table>";
 print "<tr>
 <th>Id</th>
-<th>username</th>
-<th>password</th>
+<th>Day</th>
+<th>Hour IN</th>
+<th>Hour Out</th>
 </tr>";
 //ciclo su tutti i record id`, `details`, `date_posted`, `time_posted`, `date_edited`, `time_edited`, `public`
-while ($row=mysql_fetch_assoc($res)) {
-print "<tr><td>{$row['id']}</td><td>{$row['username']}</td><td>{$row['password']}</td></tr>";
-}
-print "</table>";
-}
-//Read all of the data from the table and print it in an HTML table
-//this work only if connection is already open.
-$sql="SELECT * FROM list";
-// stampa di controllo
-echo "query SQL: $sql"."<br>";
-//esecuzione della query
-$res=mysql_query($sql,$conn);
-if (!$res)
-die("Errore query: ".mysql_error());
-
-//recupero i record inseriti (se presenti)
-$rows=mysql_num_rows($res);
-echo "<h3>Find $rows items";
-
-if ($rows==0) {
-// controllo se la risposta è vuota
-echo "<h3>No record found</h3>";
-}
-else {
-// altrimenti li visualizzo in una tabella
-print "<table>";
-print "<tr>
-<th>Id</th>
-<th>Details</th>
-<th>Post Time</th>
-<th>Edit Time</th>
-<th>Edit</th>
-<th>Delete</th>
-<th>Public Post</th>
-</tr>";
-//ciclo su tutti i record id`, `details`, `date_posted`, `time_posted`, `date_edited`, `time_edited`, `public`
-while ($row=mysql_fetch_assoc($res)) {
-print "<tr><td>{$row['id']}</td><td>{$row['details']}</td><td>{$row['date_posted']}</td><td>{$row['time_posted']}</td>
-<td>{$row['date_edited']}</td><td>{$row['time_edited']}</td><td>{$row['public']}</td>";
-print "</tr>";
+while ($row=mysql_fetch_array($res)) {
+print "<tr><td>{$row[1]}</td><td>{$row[2]}</td><td>{$row[3]}</td><td>{$row[4]}</td></tr>";
 }
 print "</table>";
 }
