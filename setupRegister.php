94ac88dee25729f3aa85b895b1c8e016be59ab2f
mpietro40 <pietromaffi@rocketmail.com>
2014-05-18 15:51:15 +0200

    Update setupRegister.php

--- setupRegister.php
+++ setupRegister.php
@@ -1,6 +1,7 @@
+<!doctype html>
 <html>
-    <head>
-	<style>
+<head>
+<style>
 .codice{
 color:red;
 font-size:16px;
@@ -17,49 +18,16 @@ color:white;
 table{
 border-collapse:collapse;
 }
-.double{
-  width:20px;
-}
 </style>
-        <title>My first PHP Website</title>
-	
-	<script>
-    function myFunction(id)
-    {
-       var r = confirm("Are you sure you want to delete this record?");
-       if(r == true)
-       {
-          window.location.assign("delete.php?id=" + id);
-       }
-    }
-</script>
-    </head>
-	
-	
-   <?php
-   session_start(); //starts the session
-   if($_SESSION['user']){ // checks if the user is logged in  
-   }
-   else{
-      header("location: index.php"); // redirects if user is not logged in
-   }
-   $user = $_SESSION['user']; //assigns user value
-   ?>
-    <body>        
-<?php 
-echo "<h2>Home Page $user</h2>";
-?>
- <!--Display's user name-->
-        <a href="logout.php">Click here to go logout</a><br/><br/>
-        <form action="add.php" method="POST">
-           Add more to list: <input type="text" name="details" /> <br/>
-           Public post? <input type="checkbox" name="public[]" value="yes" /> <br/>
-           <input type="submit" value="Add to list"/>
-        </form>
-    <h2>My presence</h2>
-   <?php
-   //check when I'm present
-   try{
+<title>Create Database</title>
+</head>
+
+<body>
+<h2>SetupDB</h2>
+<a href="index.php">Go to Home page</a>
+<?php
+try
+{
 //connect the database.
 $conn=mysql_connect("localhost","root","");
 echo "<h3>DB connesso</h3>";
@@ -71,34 +39,53 @@ if (!$ok)
 die("<br>Unable to select DB: ".mysql_error());
 else
 echo "<h3>Selected the DB first_db</h3>";
-//retrieve the user_id from username
-echo "<h3>Select from DB the user_id</h3>";
+
+echo "<h3>Prepare DB creating table presenze</h3>";
 //Add a new table to the database called users
-$sql="select id from users where username = '$user'";
+$sql="CREATE TABLE IF NOT EXISTS presence (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  id_users varchar(50) NOT NULL,
+  data Date NOT NULL,
+  hour_in varchar(8) NOT NULL default '08:30',
+  hour_out varchar(8) NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB AUTO_INCREMENT=2";
+
 // stampa di controllo
 echo "<h4>Query SQL: $sql</h4>";
 //esecuzione della query
-$res= mysql_query($sql,$conn);
-if (!$res)
+$ok= mysql_query($sql,$conn);
+if (!$ok)
 die("Error query: ".mysql_error());
 else{
-$row=mysql_fetch_array($res);
-$user_id=$row[0];
-echo "<h2>Retrieved user_id: $user_id</h2>";
+echo "<h2>Table presence created</h2>";
 }
-// -----------------------
-echo "<h3>Select from DB all the user presence</h3>";
-//Add a new table to the database called users
-$sql="select users.username, presence.data, presence.hour_in,presence.hour_out from users,presence where users.id = presence.id_users AND users.id='$user_id'";
-// stampa di controllo
-echo "<h4>Query SQL: $sql</h4>";
+
+// Set up database insert some fake data
+echo "<h3>Insert some presence</h3>";
+$sql="INSERT INTO presence (id_users, data, hour_in,hour_out) VALUES ('1', STR_TO_DATE('11/11/2013', '%m/%d/%Y'),'08:30','13:30'),".
+"('1', STR_TO_DATE('11/12/2013', '%m/%d/%Y'),DEFAULT,'13:30'),".
+"('1', STR_TO_DATE('11/13/2013', '%m/%d/%Y'),'08:30','13:30')";
+// stampo la query che eseguiro'
+echo "<h3>Setting up presence</h3>";
+echo "$sql"."<br>";
 //esecuzione della query
-$res= mysql_query($sql,$conn);
+$ok=mysql_query($sql,$conn);
+if (!$ok)
+die("<h1>Error query: </h1>".mysql_error());
+else
+print "<h2>Inserted data</h2>";
+
+//Read all of the data from the table and print it in an HTML table
+//this work only if connection is already open.
+$sql="SELECT * FROM presence";
+// stampa di controllo
+echo "query SQL: $sql"."<br>";
+//esecuzione della query
+$res=mysql_query($sql,$conn);
 if (!$res)
-die("Error query: ".mysql_error());
-else{
-echo "<h2>Retrieved presence</h2>";
-}
+die("Errore query: ".mysql_error());
+
 //recupero i record inseriti (se presenti)
 $rows=mysql_num_rows($res);
 echo "<h3>Find $rows users";
@@ -108,28 +95,20 @@ if ($rows==0) {
 echo "<h3>No record found</h3>";
 }
 else {
-
-//Create table presence
+// altrimenti li visualizzo in una tabella
 print "<table>";
-$matrix=
+print "<tr>
+<th>Id</th>
+<th>Day</th>
+<th>Hour IN</th>
+<th>Hour Out</th>
+</tr>";
+//ciclo su tutti i record id`, `details`, `date_posted`, `time_posted`, `date_edited`, `time_edited`, `public`
 while ($row=mysql_fetch_array($res)) {
-print "<tr><td>{$row[0]}</td><td>{$row[1]}</td><td>{$row[2]}</td><td>{$row[3]}</td></tr>";
+print "<tr><td>{$row[1]}</td><td>{$row[2]}</td><td>{$row[3]}</td><td>{$row[4]}</td></tr>";
 }
 print "</table>";
 }
-
-// create table for all days of school
-
-print "<table>";
-$months=["September","October","November","December","January","February","March","April","May","June"];
-for($jj=0;$jj<count($months);$jj++){
-print "<tr><th>$months[$jj]</th>";
-for($ii=1;$ii<=31;$ii++){
-  echo "<td class='double'>$ii</td>";
-}
-print "</tr>";
-}
-print "</table>";
 
 // Close the connection before leaving
 mysql_close($conn);
@@ -139,5 +118,8 @@ echo "<h3>Close connection<h3>";
 die('Generic Error');
 }
 ?>
+<footer>
+<h3><a href="index.php">Go to Home page</a></h3>
+</footer>
 </body>
 </html>
